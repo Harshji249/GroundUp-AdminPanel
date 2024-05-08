a1e82bc9087728b0fea14af277c94e295fdba3aa
Harshji249 <hs2622411@gmail.com>
2024-05-08 23:13:41 +0530

    Add time slots in add ground

--- src/AddGround.jsx
+++ src/AddGround.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./Components/Dashboard/Sidebar";
 import { TextField, Box, Typography } from "@mui/material";
 import { TextareaAutosize as BaseTextareaAutosize } from "@mui/base/TextareaAutosize";
@@ -6,6 +6,53 @@ import { styled } from "@mui/system";
 import Upload from "./Components/Assets/upload.png";
 
 const AddGround = () => {
+  const [groundDetails, setGroundDetails] = useState({
+    name: "",
+    description: "",
+    address: "",
+    timeSlot: {},
+    image: "",
+  });
+  const [timeSlots, setTimeSlots] = useState([
+    { startTime: "", endTime: "", price: "" },
+  ]);
+
+  const handleInputChange = (index, name, value) => {
+    const updatedTimeSlots = [...timeSlots];
+    updatedTimeSlots[index] = {
+      ...updatedTimeSlots[index],
+      [name]: value
+    };
+    setTimeSlots(updatedTimeSlots);
+  };
+
+  const handleAddTimeSlot = () => {
+    setTimeSlots([...timeSlots, { startTime: '', endTime: '', price: '' }]);
+  };
+
+  const handleRemoveTimeSlot = (index) => {
+    const updatedTimeSlots = [...timeSlots];
+    updatedTimeSlots.splice(index, 1);
+    setTimeSlots(updatedTimeSlots);
+  };
+
+  const [file, setFile] = useState({});
+  const handleBrowseClick = () => {
+    // Trigger click on the hidden file input element
+    document.getElementById("fileInput").click();
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setFile(file);
+    console.log("Selected file:", file);
+    // Handle the selected file as needed
+  };
+
+  const handleChange = (e) => {
+    setGroundDetails({ ...groundDetails, [e.target.name]: e.target.value });
+  };
+
   const Textarea = styled(BaseTextareaAutosize)(
     ({ theme }) => `
     width: 90%;
@@ -14,10 +61,7 @@ const AddGround = () => {
   return (
     <div className="grid-container">
       <Sidebar />
-      <main
-        className="main-container"
-        style={{ color: "black" }}
-      >
+      <main className="main-container" style={{ color: "black" }}>
         <div
           style={{ display: "flex", justifyContent: "center", width: "65%" }}
         >
@@ -31,6 +75,9 @@ const AddGround = () => {
                 id="outlined-basic"
                 label="Ground Name"
                 variant="outlined"
+                name="name"
+                value={groundDetails.name}
+                onChange={(e) => handleChange(e)}
                 sx={{ width: "90%" }}
               />
             </div>
@@ -39,6 +86,9 @@ const AddGround = () => {
               <Textarea
                 aria-label="minimum height"
                 minRows={8}
+                name="address"
+                value={groundDetails.address}
+                onChange={(e) => handleChange(e)}
                 placeholder="Ground Address"
               />
             </div>
@@ -47,6 +97,9 @@ const AddGround = () => {
               <Textarea
                 aria-label="minimum height"
                 minRows={12}
+                name="description"
+                value={groundDetails.description}
+                onChange={(e) => handleChange(e)}
                 placeholder="Ground Description"
               />
             </div>
@@ -79,9 +132,16 @@ const AddGround = () => {
                     fontWeight: "bold",
                     cursor: "pointer",
                   }}
+                  onClick={handleBrowseClick}
                 >
                   BROWSE
                 </Typography>
+                <input
+                  id="fileInput"
+                  type="file"
+                  style={{ display: "none" }}
+                  onChange={handleFileChange}
+                />
               </Box>
               <Box>
                 <Typography>Supported formates: JPEG, PNG, JPG</Typography>
@@ -89,41 +149,85 @@ const AddGround = () => {
             </Box>
 
             <Box
-              sx={{
-                border: "2px solid #000",
-                width: "55%",
-                height: "30%",
-                marginTop: 5,
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-evenly",
-                alignItems: "center",
-              }}
-            >
-              <div style={{backgroundColor:"#91CAFF",width:"100%",height:"22%",display:"flex", justifyContent:"space-around", alignItems:'center'}}>
-                <h3>From</h3>
-                <h3>To</h3>
-                <h3>Price</h3>
-              </div>
-              <div style={{width:"100%",height:"80%",display:"flex", flexDirection:"column"}}>
-                <Box sx={{width:'100%',height:'33%',display:"flex", justifyContent:'space-around'}}>
-                <TextField sx={{ width:"100px"}} id="outlined-basic" variant="outlined" />
-                <TextField sx={{width:"100px"}} id="outlined-basic" variant="outlined" />
-                <TextField sx={{ width:"100px"}} id="outlined-basic" variant="outlined" />
-                </Box>
-                <Box sx={{width:'100%',height:'33%',display:"flex", justifyContent:'space-around'}}>
-                <TextField sx={{ width:"100px"}} id="outlined-basic" variant="outlined" />
-                <TextField sx={{width:"100px"}} id="outlined-basic" variant="outlined" />
-                <TextField sx={{ width:"100px"}} id="outlined-basic" variant="outlined" />
-                </Box>
-                <Box sx={{width:'100%',height:'33%',display:"flex", justifyContent:'space-around'}}>
-                <TextField sx={{ width:"100px"}} id="outlined-basic" variant="outlined" />
-                <TextField sx={{width:"100px"}} id="outlined-basic" variant="outlined" />
-                <TextField sx={{ width:"100px"}} id="outlined-basic" variant="outlined" />
-                </Box>
-                
-              </div>
-            </Box>
+      sx={{
+        border: "2px solid #000",
+        width: "55%",
+        height: "30%",
+        marginTop: 5,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#91CAFF",
+          width: "100%",
+          height: "22%",
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <h3>From</h3>
+        <h3>To</h3>
+        <h3>Price</h3>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          height: "80%",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        {timeSlots.map((timeSlot, index) => (
+          <Box
+            key={index}
+            sx={{
+              width: "100%",
+              height: "33%",
+              display: "flex",
+              justifyContent: "space-around",
+            }}
+          >
+            <TextField
+              name="startTime"
+              value={timeSlot.startTime}
+              onChange={(e) =>
+                handleInputChange(index, e.target.name, e.target.value)
+              }
+              sx={{ width: "100px" }}
+              id="outlined-basic"
+              variant="outlined"
+            />
+            <TextField
+              name="endTime"
+              value={timeSlot.endTime}
+              onChange={(e) =>
+                handleInputChange(index, e.target.name, e.target.value)
+              }
+              sx={{ width: "100px" }}
+              id="outlined-basic"
+              variant="outlined"
+            />
+            <TextField
+              name="price"
+              value={timeSlot.price}
+              onChange={(e) =>
+                handleInputChange(index, e.target.name, e.target.value)
+              }
+              sx={{ width: "100px" }}
+              id="outlined-basic"
+              variant="outlined"
+            />
+            <button onClick={() => handleRemoveTimeSlot(index)}>Remove</button>
+          </Box>
+        ))}
+      </div>
+      <button onClick={handleAddTimeSlot}>Add Time Slot</button>
+    </Box>
           </div>
         </div>
       </main>
